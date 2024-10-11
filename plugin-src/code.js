4780415098fc56a4da4d300812b0b2fcee6640c5
David Duyker <dduyker@gmail.com>
2024-10-11 08:05:33 -0700

    upload js

---
+++ plugin-src/code.js
@@ -0,0 +1,35 @@
+"use strict";
+function rgbToHex(r, g, b) {
+    const toHex = (n) => Math.round(n * 255)
+        .toString(16)
+        .padStart(2, "0");
+    return `#${toHex(r)}${toHex(g)}${toHex(b)}`;
+}
+function getFill(nodes) {
+    const nodesWithFills = nodes.filter((node) => "fills" in node &&
+        node.fills &&
+        node.fills.length > 0 &&
+        node.fills[0].type === "SOLID");
+    if (nodesWithFills.length <= 0) {
+        figma.notify("Please select a layer with a solid fill", {
+            timeout: 1000,
+        });
+        return undefined;
+    }
+    return nodesWithFills.map((node) => node.fills[0]);
+}
+figma.on("selectionchange", () => {
+    const fills = getFill(figma.currentPage.selection);
+    console.log("selectionchange", fills);
+    if (fills && fills[0] && "color" in fills[0]) {
+        const { r, g, b } = fills[0].color;
+        const selectionColor = rgbToHex(r, g, b);
+        console.log(selectionColor);
+        figma.ui.postMessage({
+            type: "selectionChange",
+            selectionColor: selectionColor,
+        });
+    }
+});
+// This shows the HTML page in "ui.html".
+figma.showUI(__html__, { width: 240, height: 216, themeColors: true });
